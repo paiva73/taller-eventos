551f3f2e7c1c9ba653f51265578fb2af7d108dc4
paiva73 <mau.paiva10@gmail.com>
2023-08-12 14:19:45 -0300

    Deteniendo propagación de evento click del button

--- eventos.js
+++ eventos.js
@@ -1,5 +1,10 @@
 const div = document.querySelector('div');
+const button = document.querySelector('button');
 
 div.addEventListener('click', () => {
     alert("Hola! Soy el div");
 });
+button.addEventListener('click', (event) => {
+    event.stopPropagation();
+    alert("Hola!");
+})
